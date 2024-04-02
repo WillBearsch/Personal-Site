44878a13a581090c1c1fb7779febd5cf6c2bba26
WillBearsch <william.bearsch@gmail.com>
2024-04-02 13:08:32 -0400

    initial commit

---
+++ src/utils/Themes.js
@@ -0,0 +1,12 @@
+export const darkTheme = {
+    bg:"#1C1C27",
+    bgLight:"#1C1E27",
+    primary:"#854CE6",
+    text_primary:"#F2F3F4",
+    text_secondary:"#B1B2B3",
+    card:"#171721",
+    card_light:"#191924",
+    button:"#854CE6",
+    white:"#FFFFFF",
+    black:"#000000",
+};
